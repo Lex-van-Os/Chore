1ad9c46add33c18c16c75ed844ffc6dd92802c42
Lex van Os <vanoslex7@gmail.com>
2022-08-26 11:57:14 +0200

    Implemented functional Task CRUD API that can be used using Swagger

--- ChoreApp/ChoreApp/Client/Program.cs
+++ ChoreApp/ChoreApp/Client/Program.cs
@@ -1,3 +1,5 @@
+global using ChoreApp.Client.Services.TaskService;
+global using ChoreApp.Shared;
 using ChoreApp.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +9,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ITaskService, TaskService>();
 
 await builder.Build().RunAsync();
---
+++ ChoreApp/ChoreApp/Client/Services/TaskService/ITaskService.cs
@@ -0,0 +1,12 @@
+﻿namespace ChoreApp.Client.Services.TaskService
+{
+    public interface ITaskService
+    {
+        List<ChoreApp.Shared.Task> Tasks { get; set; }
+        System.Threading.Tasks.Task GetTasks();
+        System.Threading.Tasks.Task<ChoreApp.Shared.Task> GetTask(int id);
+        System.Threading.Tasks.Task CreateTask(ChoreApp.Shared.Task task);
+        System.Threading.Tasks.Task UpdateTask(ChoreApp.Shared.Task task);
+        System.Threading.Tasks.Task DeleteTask(int id);
+    }
+}
